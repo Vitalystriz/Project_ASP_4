1ba0709485de2cb125bb1dae982775441e4a4f40
vitaly <vitalystriz@gmail.com>
2026-06-16 22:03:48 +0300

    docker container for react created and docker-compose changed

--- node-backend/app.js
+++ node-backend/app.js
@@ -18,7 +18,7 @@ app.use('/api/restaurants', restaurantsRoutes)
 app.use('/api/orders', ordersRouter)
 app.use('/api/search', search)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
